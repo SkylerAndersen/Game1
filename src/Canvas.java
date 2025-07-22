712aabf4ac78d12378fb5810314c0eb04cef1df5
SkylerAndersen <skylerandersen23@gmail.com>
2025-07-22 16:05:01 -0700

    refactored graphics. improved performance, removed flickering.

--- src/Canvas.java
+++ src/Canvas.java
@@ -6,27 +6,22 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 public class Canvas {
-    public static final int SAND_WORLD = 0;
-    public static final int MIDDLE_WORLD = 1;
-    public static final int FOREST_WORLD = 2;
-    public static final int BASE_POSE = 3;
-    public static final int WALK_POSE = 4;
-    public static final int ATTACK_POSE = 5;
-    public static final int MAIN_CHARACTER = 6;
-    public static final int ALT_CHARACTER = 7;
+    public static final int SAND_WORLD = 0, MIDDLE_WORLD = 1, FOREST_WORLD = 2, BASE_POSE = 3, WALK_POSE = 4,
+            ATTACK_POSE = 5, MAIN_CHARACTER = 6, ALT_CHARACTER = 7;
     private final FileHandler fileHandler = FileHandler.get();
     private final InputManager inputManager = InputManager.get();
     private JPanel applicationScreen;
     private long previousCycleTime;
     private Image background;
     private Image character, walkPose, attackPose, altCharacter, altWalkPose, altAttackPose;
-    private JLabel previousWorldImage;
-    private JLabel previousCharacterImage;
+    private JLabel wrapper, wrapper2;
 
     public Canvas (JPanel applicationScreen) {
         this.applicationScreen = applicationScreen;
         previousCycleTime = System.currentTimeMillis();
-        applicationScreen.setLayout(new LayeredLayoutManager(applicationScreen));
+        wrapper = new JLabel();
+        wrapper2 = new JLabel();
+        applicationScreen.setLayout(new ScreenLayoutManager(applicationScreen));
     }
     public void loadWorld (int worldNum) {
         String name = worldNum == 0 ? "sand-world.png" : worldNum == 1 ? "middle-world.png" :
@@ -39,6 +34,7 @@ public class Canvas {
         character = fileHandler.readImage("base-pose.png");
         walkPose = fileHandler.readImage("walk-pose.png");
         attackPose = fileHandler.readImage("attack-pose.png");
+        drawCharacterFromCoordinates(characterNum,Canvas.BASE_POSE,0,0,true);
     }
 
     public void refreshCycle () {
@@ -46,8 +42,8 @@ public class Canvas {
             @Override
             public void run() {
                 while (true) {
-                    drawWorldFromCoordinates(0,0);
-                    drawCharacterFromCoordinates(MAIN_CHARACTER,BASE_POSE,0,0,true);
+//                    drawWorldFromCoordinates(0,0);
+//                    drawCharacterFromCoordinates(MAIN_CHARACTER,BASE_POSE,0,0,true);
                     applicationScreen.repaint();
                     applicationScreen.revalidate();
 
@@ -63,21 +59,14 @@ public class Canvas {
     }
 
     public void drawWorldFromCoordinates (int x, int y) {
-        if (previousWorldImage == null)
-            previousWorldImage = new JLabel();
-
-        int width = (int)applicationScreen.getParent().getSize().getWidth();
-        int height = (int)applicationScreen.getParent().getSize().getHeight();
-        width = width == 0 ? 1000 : width;
-        height = height == 0 ? 800 : height;
-        Image image = background.getScaledInstance(width,height, Image.SCALE_FAST);
         Integer[] constraints = new Integer[3];
-        constraints[0] = 0;
+        constraints[0] = ScreenLayoutManager.BACKGROUND;
         constraints[1] = x;
         constraints[2] = y;
-        previousWorldImage.setIcon(new ImageIcon(image));
-        previousWorldImage.setSize(new Dimension(width,height));
-        applicationScreen.add(previousWorldImage,constraints);
+        wrapper.setIcon(new ImageIcon(background));
+        wrapper.setSize(new Dimension(background.getWidth(null),
+                background.getHeight(null)));
+        applicationScreen.add(wrapper,constraints);
     }
 
     public void drawCharacterFromCoordinates (int character, int pose, int x, int y) {
@@ -85,9 +74,6 @@ public class Canvas {
     }
 
     public void drawCharacterFromCoordinates (int character, int pose, int x, int y, boolean flipped) {
-        if (previousCharacterImage == null)
-            previousCharacterImage = new JLabel();
-
         // select image
         Image drawing = (character == MAIN_CHARACTER) ? switch (pose) {
             case ATTACK_POSE -> attackPose;
@@ -123,11 +109,12 @@ public class Canvas {
         int width = drawing.getWidth(null);
         int height = drawing.getHeight(null);
         Integer[] constraints = new Integer[3];
-        constraints[0] = 1;
+        constraints[0] = ScreenLayoutManager.CHARACTER;
         constraints[1] = x;
         constraints[2] = y;
-        previousCharacterImage.setIcon(new ImageIcon(drawing));
-        previousCharacterImage.setSize(new Dimension(width,height));
-        applicationScreen.add(previousCharacterImage,constraints);
+        wrapper2.setIcon(new ImageIcon(drawing));
+        wrapper2.setSize(new Dimension(width,height));
+//        applicationScreen.remove(previousCharacterImage);
+        applicationScreen.add(wrapper2,constraints);
     }
 }
---
+++ src/ScreenLayoutManager.java
@@ -0,0 +1,176 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+
+public class ScreenLayoutManager implements LayoutManager, LayoutManager2 {
+    public static final Integer BACKGROUND = 0, CHARACTER = 1, ITEM = 2;
+    private Container parent;
+    private HashMap<Component,BufferedImage> graphicsElements;
+    private HashMap<Component,Integer> layer;
+    private HashMap<Component,int[]> coordinates;
+    private int bgWidth, bgHeight;
+    private JLabel imageContainer;
+
+    public ScreenLayoutManager(Container parent) {
+        this.parent = parent;
+        layer = new HashMap<>();
+        coordinates = new HashMap<>();
+        graphicsElements = new HashMap<>();
+        imageContainer = new JLabel();
+        bgWidth = 0;
+        bgHeight = 0;
+        parent.add("Container",imageContainer);
+    }
+
+    /**
+     * Not Implemented in this LayoutManager.
+     * */
+    @Override
+    public void addLayoutComponent(String name, Component comp) {}
+
+    @Override
+    public void removeLayoutComponent(Component comp) {
+        if (!(comp instanceof JLabel) || ((JLabel) comp).getIcon() == null)
+            return;
+        layer.remove(comp);
+        coordinates.remove(comp);
+        graphicsElements.remove(comp);
+    }
+
+    @Override
+    public Dimension preferredLayoutSize(Container parent) {
+        boolean sizeInitialized = parent.getSize().width == 0 || parent.getSize().height == 0;
+        return sizeInitialized ? parent.getSize() : parent.getPreferredSize();
+    }
+
+    @Override
+    public Dimension minimumLayoutSize(Container parent) {
+        return parent.getMinimumSize();
+    }
+
+    @Override
+    public void layoutContainer(Container parent) {
+        int width = parent.getSize().width;
+        int height = parent.getSize().height;
+        double scaleFactor = (double)height / bgHeight;
+//        System.out.printf("Height: %d, bgHeight: %d\n",height,bgHeight);
+
+        // generate image to draw
+        BufferedImage canvas = new BufferedImage(bgWidth,bgHeight,BufferedImage.TYPE_INT_ARGB);
+        for (Component imageComponent : graphicsElements.keySet()) {
+            if (layer.get(imageComponent).equals(ScreenLayoutManager.BACKGROUND)) {
+                int x = coordinates.get(imageComponent)[0];
+                int y = coordinates.get(imageComponent)[1];
+                BufferedImage image = graphicsElements.get(imageComponent);
+                int currWidth = image.getWidth();
+                int currHeight = image.getHeight();
+                for (int i = 0; i < currWidth && i + x < bgWidth; i++) {
+                    for (int j = 0; j < currHeight && j + x < bgHeight; j++) {
+                        canvas.setRGB(i+x,j+y,image.getRGB(i,j));
+                    }
+                }
+            }
+            canvas.flush();
+        }
+        for (Component imageComponent : graphicsElements.keySet()) {
+            if (layer.get(imageComponent).equals(ScreenLayoutManager.CHARACTER)) {
+                int x = coordinates.get(imageComponent)[0];
+                int y = coordinates.get(imageComponent)[1];
+                BufferedImage image = graphicsElements.get(imageComponent);
+                int currWidth = image.getWidth();
+                int currHeight = image.getHeight();
+                for (int i = 0; i < currWidth && i + x < bgWidth; i++) {
+                    for (int j = 0; j < currHeight && j + x < bgHeight; j++) {
+                        canvas.setRGB(i+x,j+y,image.getRGB(i,j));
+                    }
+                }
+            }
+            canvas.flush();
+        }
+        for (Component imageComponent : graphicsElements.keySet()) {
+            if (layer.get(imageComponent).equals(ScreenLayoutManager.ITEM)) {
+                int x = coordinates.get(imageComponent)[0];
+                int y = coordinates.get(imageComponent)[1];
+                BufferedImage image = graphicsElements.get(imageComponent);
+                int currWidth = image.getWidth();
+                int currHeight = image.getHeight();
+                for (int i = 0; i < currWidth && i + x < bgWidth; i++) {
+                    for (int j = 0; j < currHeight && j + x < bgHeight; j++) {
+                        canvas.setRGB(i+x,j+y,image.getRGB(i,j));
+                    }
+                }
+            }
+            canvas.flush();
+        }
+
+        // scale image
+        int scaledWidth = (int)(bgWidth * scaleFactor);
+        int scaledHeight = (int)(bgHeight * scaleFactor);
+//        System.out.println(""+bgWidth+" "+bgHeight+" "+scaleFactor+" ");
+        ImageIcon toDraw = new ImageIcon(canvas.getScaledInstance(scaledWidth,
+                scaledHeight,Image.SCALE_FAST));
+
+        // draw image
+//        System.out.printf("Scaled Width: %d, Scaled Height: %d\n",scaledWidth,scaledHeight);
+        int labelX = (width - scaledWidth) / 2;
+        int labelY = 0;
+        imageContainer.setBounds(labelX,labelY,scaledWidth,scaledHeight);
+        imageContainer.setIcon(toDraw);
+//        System.out.printf("LabelX: %d, LabelY: %d\n",labelX,labelY);
+    }
+
+    @Override
+    public void addLayoutComponent(Component comp, Object constraints) {
+        if (!(constraints instanceof Integer[]))
+            throw new RuntimeException("Invalid Constrains Parameter.");
+
+        Integer[] layerXY = (Integer[])constraints;
+        if (layerXY.length != 3)
+            throw new RuntimeException("Invalid Constrains Parameter.");
+
+        if (!(comp instanceof JLabel) || ((JLabel) comp).getIcon() == null)
+            return;
+
+        // get the image properties
+        layer.put(comp,layerXY[0]);
+        int[] XY = {layerXY[1],layerXY[2]};
+        coordinates.put(comp,XY);
+
+        // get the image itself
+        java.awt.Image image = ((ImageIcon) ((JLabel) comp).getIcon()).getImage();
+        BufferedImage graphicsElement = new BufferedImage(image.getWidth(null),
+                image.getHeight(null),BufferedImage.TYPE_INT_ARGB);
+        graphicsElement.getGraphics().drawImage(image,0,0,null);
+        graphicsElements.put(comp,graphicsElement);
+
+        // get the width and height of the background
+        if (graphicsElement.getWidth() > bgWidth)
+            bgWidth = graphicsElement.getWidth();
+        if (graphicsElement.getHeight() > bgHeight)
+            bgHeight = graphicsElement.getHeight();
+    }
+
+    @Override
+    public Dimension maximumLayoutSize(Container target) {
+        return target.getMaximumSize();
+    }
+
+    @Override
+    public float getLayoutAlignmentX(Container target) {
+        return target.getAlignmentX();
+    }
+
+    @Override
+    public float getLayoutAlignmentY(Container target) {
+        return target.getAlignmentY();
+    }
+
+    /**
+     * Not Implemented in this LayoutManager.
+     * */
+    @Override
+    public void invalidateLayout(Container target) {
+    }
+}
