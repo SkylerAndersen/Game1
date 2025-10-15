859f8877dfd9e2d502970774aeb61a87092c641e
SkylerAndersen <skylerandersen23@gmail.com>
2025-10-15 21:33:02 +0200

    Swapped LockSupport.park() for Object.wait() to make use of smaller synchronous blocks that limit the time threads hold competing locks on the MinHeap. Also removed ensureShutdown(). Didn't work well and I realized I had no usecase.

--- src/AsynchronousDispatch.java
+++ src/AsynchronousDispatch.java
@@ -1,67 +1,61 @@
 import DataManagement.MinHeap;
 
-import java.util.concurrent.locks.LockSupport;
-
 public class AsynchronousDispatch {
-    private Thread dispatchThread;
-    private MinHeap<Runnable> queuedTasks;
+    private final Thread dispatchThread;
+    private final MinHeap<Runnable> queuedTasks = new MinHeap<>();
     private boolean threadRunning, initialWakeup;
-    private long programEpoch;
+    private final Object lock = new Object();
+    private final long programEpoch = System.currentTimeMillis();
     public AsynchronousDispatch () {
-        programEpoch = System.currentTimeMillis();
-        queuedTasks = new MinHeap<>();
         threadRunning = true;
         initialWakeup = false;
-
-        dispatchThread = new Thread() {
-            @Override
-            public void run () {
-                while (threadRunning) {
-                    System.out.println("DispatchThread looped in: "+Thread.currentThread().getName());
-                    executeTasks();
-                    if (queuedTasks.size() == 0) {
-                        System.out.println("Empty. Parking thread: "+Thread.currentThread().getName());
-                        LockSupport.park();
-                    } else {
-                        long nextWakeupTime = (queuedTasks.peek().getVal1()+programEpoch);
-                        System.out.println("Non-Empty. Parking thread: "+Thread.currentThread().getName());
-                        System.out.println("Non-Empty. Parking thread until: "+nextWakeupTime);
-                        LockSupport.parkUntil(nextWakeupTime);
-                    }
-                }
-            }
-        };
+        dispatchThread = new Thread(this::dispatchThreadUpdateLoop,"customDispatchThread");
     }
 
-    private synchronized void executeTasks () {
-        System.out.println("Execute Tasks called from: "+Thread.currentThread().getName());
-        while (queuedTasks.size() != 0 && (queuedTasks.peek().getVal1())+programEpoch <=
-                System.currentTimeMillis()) {
-            Runnable task = queuedTasks.pop().getVal2();
+    private void dispatchThreadUpdateLoop () {
+        while (threadRunning) {
+//            System.out.println("DispatchThread looped in: "+Thread.currentThread().getName());
+            Runnable task;
+            synchronized (lock) {
+                while (queuedTasks.size() == 0) {
+//                    System.out.println("Empty. Parking thread: " + Thread.currentThread().getName());
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                }
+                while (true) {
+                    long nextWakeupTime = (queuedTasks.peek().getVal1() + programEpoch);
+                    long currTime = System.currentTimeMillis();
+                    long timeLeft = nextWakeupTime-currTime;
+                    if (timeLeft <= 0)
+                        break;
+                    try {
+                        lock.wait(timeLeft);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+//                    System.out.println("Non-Empty. Parking thread: " + Thread.currentThread().getName());
+//                    System.out.println("Non-Empty. Parking thread until: " + nextWakeupTime);
+                }
+                task = queuedTasks.pop().getVal2();
+            }
             task.run();
         }
     }
 
-    public synchronized void schedule (int delay, Runnable task) {
+    public void schedule (int delay, Runnable task) {
         long time = System.currentTimeMillis();
         int timeSinceEpoch = (int)(time-programEpoch);
-        System.out.println("Time is: "+time);
-        System.out.println("Time since epoch is: "+timeSinceEpoch);
-        System.out.println("Schedule method schedules task for: "+(timeSinceEpoch+delay));
-        queuedTasks.push(timeSinceEpoch+delay,task);
-        LockSupport.unpark(dispatchThread);
-    }
-
-    public synchronized void ensureShutdown () {
-        if (!threadRunning && initialWakeup)
-            return;
-
-        threadRunning = false;
-        LockSupport.unpark(dispatchThread);
-        try {
-            dispatchThread.join();
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to ensure the shutdown.");
+//        System.out.println("Time is: "+time);
+//        System.out.println("Time since epoch is: "+timeSinceEpoch);
+//        System.out.println("Schedule method schedules task for: "+(timeSinceEpoch+delay));
+        synchronized (lock) {
+            queuedTasks.push(timeSinceEpoch+delay,task);
+            lock.notify();
         }
     }
 
@@ -72,6 +66,7 @@ public class AsynchronousDispatch {
         dispatchThread.start();
         while (!dispatchThread.isAlive()) {}
         initialWakeup = true;
+        System.out.println("Dispatch active");
     }
 
 }
