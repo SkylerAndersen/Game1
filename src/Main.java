9ed0c74a19fd5867c55cb44900b93431ba27d93b
SkylerAndersen <skylerandersen23@gmail.com>
2025-10-17 17:25:33 +0200

    removed original file paths from tracking.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+import ApplicationManagement.Application;
+
 public class Main {
     public static void main (String[] args) {
         Application application = Application.get();
         application.start();
-//        application.start();
     }
 }
