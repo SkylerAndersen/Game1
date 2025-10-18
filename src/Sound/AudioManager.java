54b51b29aeb80f185e96a87904076a77a297294a
SkylerAndersen <skylerandersen23@gmail.com>
2025-10-18 12:17:01 +0200

    Fixed sound playback speed, and added new track.

--- src/Sound/AudioManager.java
+++ src/Sound/AudioManager.java
@@ -12,7 +12,21 @@ public class AudioManager {
     private byte[] backgroundMusic;
     private Thread audioThread;
     private AudioManager () {
-        backgroundMusic = fileHandler.readAudio("soundtrack.wav");
+        byte[] backgroundMusicOriginal = fileHandler.readAudio("soundtrack2.wav");
+        backgroundMusic = new byte[backgroundMusicOriginal.length/2];
+        for (int i = 0; i < backgroundMusic.length; i+=2) {
+            byte byte1 = backgroundMusicOriginal[i*2];
+            byte byte2 = backgroundMusicOriginal[i*2+1];
+            byte byte3 = backgroundMusicOriginal[i*2+2];
+            byte byte4 = backgroundMusicOriginal[i*2+3];
+            int sample1 = ((byte1 << 8) & 0x0000FF00) | ((int)(byte2) &0x000000FF);
+            int sample2 = ((byte3 << 8) & 0x0000FF00) | ((int)(byte4) &0x000000FF);
+            int unifiedSample = (sample1+sample2)/2;
+            byte newSample1 = (byte) ((unifiedSample >> 8) & 0x000000FF);
+            byte newSample2 = (byte) (unifiedSample & 0x000000FF);
+            backgroundMusic[i] = newSample1;
+            backgroundMusic[i+1] = newSample2;
+        }
         audioThread = new Thread(this::audioTask,"audioThread");
         audioThread.start();
     }
@@ -31,7 +45,8 @@ public class AudioManager {
             sourceDataLine.open(format);
             sourceDataLine.start();
             sourceDataLine.write(backgroundMusic,0,backgroundMusic.length);
-            sourceDataLine.flush();
+            sourceDataLine.drain();
+            sourceDataLine.stop();
             sourceDataLine.close();
         } catch (Exception e) {
             e.printStackTrace();
