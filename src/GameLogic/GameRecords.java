85866bf35d59b838c3c24f0d73c5e4739c4ef93c
SkylerAndersen <skylerandersen23@gmail.com>
2025-12-01 18:43:46 +0100

    Added GameRecords to organize shared fields.

---
+++ src/GameLogic/GameRecords.java
@@ -0,0 +1,7 @@
+package GameLogic;
+
+public class GameRecords {
+    public static int worldOffsetX, worldOffsetY, characterX, characterY;
+    public static boolean flippedCharacter = false;
+    public static int characterPose = Character.BASE_POSE;
+}
